7f71d15be0fe9c17fcc803d2225d1b77e44fcb66
sdey81 <wsh.ninth12@gmail.com>
2017-04-07 01:43:36 +0800

    first commit

---
+++ MrrCard/MrrCard/Views/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace MrrCard.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
